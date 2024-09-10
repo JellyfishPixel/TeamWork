6bac273644a2f3749b8d597bcc951d87d37e1933
Mesa123456789 <Masapimonpat@gmail.com>
2024-09-10 14:31:46 +0700

    Update Game1.cs

--- TeamWork/Game1.cs
+++ TeamWork/Game1.cs
@@ -9,6 +9,7 @@ namespace TeamWork
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         int x = 0;
+        int y = 0;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
